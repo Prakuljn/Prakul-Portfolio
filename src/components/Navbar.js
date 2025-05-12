2c5088cae4323919fca77460fa87480117718709
AnshulMenaria <anshulmenaria@gmail.com>
2025-05-12 16:57:00 +0530

    Photo Update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,7 +94,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/AnshulMenaria/Portfolio1"
+                href="https://github.com/Prakuljn/Prakul-Portfolio.git"
                 target="_blank"
                 className="fork-btn-inner"
               >
